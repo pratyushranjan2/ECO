2ea7b830262313a5a4ab1c56f5601069788e9a25
* <*pratyushranjan02@gmail.com*>
2022-03-31 18:19:24 +0530

    Added sortby feature

--- server.py
+++ server.py
@@ -16,10 +16,14 @@ app = Flask(__name__)
 fe = FeatureExtractor()
 model = Load_DeepFashion2_Yolov3()
 max_matches = 6
+packets = []
+uploaded_img_path = None
 
 meta_hoodie = ['S M L', 'S M', 'M L', 'S M L XL', 'L', 'S M L']
 meta_footwear = ['6 7 8 9 10', '8 9 10', '6 7 8', '8', '6 10', '11']
+location = ['Hyderabad', 'Hyderabad', 'Bangalore', 'Pune', 'Bangalore', 'Pune']
 costs = ['\u20B9 300','\u20B9 400','\u20B9 350','\u20B9 450','\u20B9 250','\u20B9 300',]
+rating = [4, 5, 4, 3, 4, 4]
 
 with open('footwear_features.pkl', 'rb') as f:
     footwear_features = pickle.load(f)
@@ -28,20 +32,12 @@ with open('nike_men_hoodie_features.pkl', 'rb') as f:
 
 
 def getScores(img_url):
+    global uploaded_img_path
+    global packets
     response = requests.get(img_url)
     img = Image.open(BytesIO(response.content))
     uploaded_img_path = "static/uploaded/" + datetime.now().isoformat().replace(":", ".") + "_" + 'sample.jpg'
     img.save(uploaded_img_path)
-
-    # if hoodie:
-    #     features = nike_men_hoodie_features
-    #     img_tensor = Read_Img_2_Tensor(uploaded_img_path)
-    #     img_crop = Detect_Clothes_and_Crop(img_tensor, model)
-    #     Save_Image(img_crop, uploaded_img_path)
-    #     img = Image.open(uploaded_img_path)
-    
-    # else:
-    #     features = footwear_features
 
     img_tensor = Read_Img_2_Tensor(uploaded_img_path)
     img_crop, detected = Detect_Clothes_and_Crop(img_tensor, model)
@@ -49,8 +45,10 @@ def getScores(img_url):
     if detected:
         features = nike_men_hoodie_features
         Save_Image(img_crop, uploaded_img_path)
+        hoodie = True
     else:
         features = footwear_features
+        hoodie = False
     
     img = Image.open(uploaded_img_path)
     
@@ -62,17 +60,50 @@ def getScores(img_url):
             scores[path] = f'{nn.CosineSimilarity(dim=0)(target_vector, vector).item()*100: .2f}'
 
         scores = [(score,url) for url,score in sorted(scores.items(), key = lambda item: item[1], reverse=True)][0:max_matches]
-    
-    return scores, uploaded_img_path
+
+        packets.clear()
+        for i in range(len(scores)):
+            packets.append({'url': scores[i][1],
+                            'score': scores[i][0],
+                            'sizes': meta_hoodie[i] if hoodie else meta_footwear[i],
+                            'cost': costs[i],
+                            'location': location[i],
+                            'rating': rating[i]
+                            })
+
+@app.route('/sort', methods=['POST'])
+def sort():
+    global uploaded_img_path
+    global packets
+    param = request.form.get('param')
+    if param is not None:
+        if param == 'cost':
+            new_packets = sorted(packets, key = lambda d: d['cost'])
+        elif param == 'location':
+            new_packets = sorted(packets, key = lambda d: d['location'])
+        elif param == 'score':
+            new_packets = sorted(packets, key = lambda d: d['score'], reverse=True)
+        else:
+            print('sort param not identified')
+            new_packets = packets
+        return render_template('card2.html',
+                            query_path = uploaded_img_path,
+                            packets = new_packets)
+    else:
+        print(f'unable to receive sorting parameter')
+        return render_template('card2.html')
 
 @app.route('/extsearch', methods=['GET'])
 def searchByUrl():
+    global uploaded_img_path
+    global packets
     url = request.args.get('url')
     if (url is not None):
-        scores, uploaded_img_path = getScores(url)
+        getScores(url)
+        print(f'uploaded_img_path={uploaded_img_path}')
         return render_template('card2.html',
                                query_path=uploaded_img_path,
-                               scores=zip(scores, meta_hoodie, costs))
+                               packets=packets)
     else:
         print('url not received')
         return render_template('card2.html')
@@ -92,12 +123,12 @@ def index():
             img_url = vals[2]
             hoodie = True
 
-        scores, uploaded_img_path = getScores(img_url)
+        getScores(img_url)
         meta = meta_hoodie if hoodie else meta_footwear
 
         return render_template('card2.html',
                                query_path=uploaded_img_path,
-                               scores=zip(scores, meta, costs)
+                               packets=packets
                                )
 
     else:
